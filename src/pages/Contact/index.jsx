7d164d01ca7c898bc1d4c848273d8ac4bca77247
jawad zafari <jawad.zafari@laplateforme.io>
2026-07-30 10:54:43 +0200

    Ajouter le bouton (mail to)

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,9 +1,21 @@
 import PageContent from "../../components/PageContent";
 
 export default function Contact() {
+  const targetEmail = "contact@example.com";
+  const mailtoUrl = `mailto:${targetEmail}`;
+
+  const handleButtonClick = () => {
+    window.open(mailtoUrl, "_blank");
+  };
+
   return (
-    <>
+    <div>
       <PageContent slug="contact" />
-    </>
+      <div>
+        <button type="button" onClick={handleButtonClick}>
+          Envoyer un e-mail
+        </button>
+      </div>
+    </div>
   );
 }
