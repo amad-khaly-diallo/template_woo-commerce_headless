b33b38107c101c5d38a6ec86a3523b497a4562de
cosmin-bilga <cosmin.bilga@laplateforme.io>
2026-08-26 13:43:03 +0200

    swapped price and stock position on product card

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -86,13 +86,13 @@ export default function ProductCard({ product }) {
 
       <div className="description">
         <span>
-          <span>Prix: </span>
-          <span dangerouslySetInnerHTML={{ __html: product.price_html }} />
           {product.is_in_stock ? (
             <p className="stock-status-available">En stock</p>
           ) : (
             <p className="stock-status-unavailable">Rupture de stock</p>
           )}
+          <span>Prix: </span>
+          <span dangerouslySetInnerHTML={{ __html: product.price_html }} />
         </span>
 
         <span>
