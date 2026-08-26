f08758805d74c50f1d53957692bcf4bf01c04324
nordine-aitouaraz <nordine.ait-ouaraz@laplateforme.io>
2026-08-26 16:23:21 +0200

    background

--- src/pages/Store/index.jsx
+++ src/pages/Store/index.jsx
@@ -1,11 +1,25 @@
+import { useSelector } from "react-redux";
 import Feed from "../../components/Feed";
 import Filters from "../../components/Filters";
+import "./index.css";
 
+const BACKGROUNDS = {
+  femme: "/backgrounds/femme.jpg",
+  homme: "/backgrounds/homme.jpg",
+  mixte: "/backgrounds/mixte.jpg",
+};
 export default function Store() {
+  const catId = useSelector((state) => state.filters.category);
+  const categories = useSelector((state) => state.categories.items);
+  const slug = categories.find((cat) => cat.id.toString() === catId)?.slug;
+  const bg = BACKGROUNDS[slug];
+
   return (
-    <main>
+    <main className="store-page">
       <Filters />
-      <Feed />
+      <div className="category-bg" style={{ "--cat-bg": bg ? `url(${bg})` : "none" }}>
+        <Feed />
+      </div>
     </main>
   );
 }
