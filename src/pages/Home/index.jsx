8f060f2e018fef2bf4547b38f3b52b23a56ec1de
Youssef-Touria <touria.youssef@laplateforme.io>
2026-08-26 16:41:00 +0200

    ajout du composant Advantages sur la page Home"

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import HomeSlider from "../../components/HomeSlider";
 import PageContent from "../../components/PageContent";
 import DeleteAccountButton from "../../components/DeleteAccountButton";
+import Advantages from "../../components/Advantages";
 
 export default function Home() {
   return (
     <div className="home">
       <PageContent slug="home" />
       <HomeSlider />
+       <Advantages />
       <PageContent slug="a-propos" />
     </div>
   );
