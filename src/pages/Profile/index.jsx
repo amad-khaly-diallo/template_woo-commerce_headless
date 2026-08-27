192706f263e609421e6f3fe23f0484269fe24de2
pierre-canet <pierre.canet@laplateforme.io>
2026-08-27 09:28:53 +0200

    changements mineurs au code et ajout d'un CSS pour la page profil

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -5,6 +5,7 @@ import DeleteAccountButton from "../../components/DeleteAccountButton";
 import { UserDisplay } from "../../components/UserDisplay";
 import { OrderAll } from "../../components/OrderAll";
 import { useEffect } from "react";
+import "./index.css";
 
 export default function Profile() {
   const isAuthentificated = useSelector((state) => state.user?.token);
@@ -15,7 +16,7 @@ export default function Profile() {
   const navigate = useNavigate();
   if (isAuthentificated) {
     return (
-      <div>
+      <div className="test">
         <UserDisplay />
         <OrderAll />
         <DeleteAccountButton />
